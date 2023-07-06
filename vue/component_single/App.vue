b7b23c341c19e38a42cc4040ab23cde2aa5e8ad2
sanbuks <sandbuks@gmail.com>
2023-07-06 08:37:18 +0800

    feat(vue): update

---
+++ vue/component_single/App.vue
@@ -0,0 +1,19 @@
+<script>
+import School from './School.vue'
+import Student from './Student.vue'
+
+export default {
+  name: 'App',
+  components : {
+    School,
+    Student
+  }
+}
+</script>
+
+<template>
+<div>
+  <School></School>
+  <Student></Student>
+</div>
+</template>
