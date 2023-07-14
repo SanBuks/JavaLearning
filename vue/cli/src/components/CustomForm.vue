55b9263948cdc770bf0a6fcc9914b50c749305fd
fyh <sanbuks@outlook.com>
2023-07-14 18:05:32 +0800

    feat(vue): update

---
+++ vue/cli/src/components/CustomForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <label>子组件信息</label>
+    <input type="text" placeholder="请输入信息" v-model="msg" @input="$emit('input', msg)">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CustomForm",
+  data() {
+    return {
+      msg: this.value
+    }
+  },
+  watch: {
+    value: {
+      deep: true,
+      handler(newValue) {
+        this.msg = newValue
+      }
+    },
+  },
+  props:["value"]
+}
+</script>
+
--- vue/cli/src/constants/bus_event_name.js
+++
@@ -1,9 +0,0 @@
-const EVENT_CHANGE_ITEM = "EventChangeItem"
-const EVENT_REMOVE_ITEM = "EventRemoveItem"
-const EVENT_EDIT_ITEM = "EventEditItem"
-
-export {
-  EVENT_CHANGE_ITEM,
-  EVENT_REMOVE_ITEM,
-  EVENT_EDIT_ITEM
-}
