151a151d11deaeef0a2fbe040baab05f3246d0ab
fyh <sanbuks@outlook.com>
2023-08-14 20:26:17 +0800

    feat(java): update

---
+++ java_core_01/ch12-juc/src/main/java/producer_consumer/Consumer.java
@@ -0,0 +1,22 @@
+package producer_consumer;
+
+public class Consumer extends Thread {
+    Clerk clerk;
+
+    public Consumer(String name, Clerk clerk) {
+        super(name);
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            clerk.removeProduct(getName());
+        }
+    }
+}
