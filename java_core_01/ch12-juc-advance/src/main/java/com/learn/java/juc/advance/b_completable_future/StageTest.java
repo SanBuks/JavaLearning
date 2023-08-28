bb329ed5bef6c251cca7218e57d62988765b24a7
fyh <sanbuks@outlook.com>
2023-08-28 18:05:33 +0800

    feat(java): update

--- java_core_01/ch12-juc-advance/src/main/java/com/learn/java/juc/advance/b_completable_future/StageTest.java
+++ java_core_01/ch12-juc-advance/src/main/java/com/learn/java/juc/advance/b_completable_future/StageTest.java
@@ -2,11 +2,40 @@ package com.learn.java.juc.advance.b_completable_future;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class StageTest {
+
+    @Test
+    public void getTest() throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        CompletableFuture<String> result = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "success";
+        }, pool);
+
+        // System.out.println(result.get(4, TimeUnit.SECONDS));
+        System.out.println(result.join());
+    }
+
+    @Test
+    public void completeTest() throws ExecutionException, InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        CompletableFuture<String> result = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "success";
+        }, pool);
+
+        System.out.println(result.complete("test") + "\t" + result.get());
+    }
 
     @Test
     public void thenApplyTest() {
@@ -24,14 +53,169 @@ public class StageTest {
             if (e == null) {
                 System.out.println(r);
             }
-        }).exceptionally((e)->{
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+        }).exceptionally((e) -> {
+            System.out.println("@" + e.getMessage());
             return null;
         });
         pool.shutdown();
     }
 
+    @Test
+    public void handlerTest() {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                System.out.println("111");
+                int a_ = 1 / 0;
+                return 1;
+            }, pool).handle((r, e) -> {
+                if (e == null) {
+                    System.out.println("222");
+                    return r + 1;
+                } else {
+                    System.out.println(e.getMessage());
+                    throw new RuntimeException((e.getMessage()));
+                }
+            }).handle((r, e) -> {
+                if (e == null) {
+                    System.out.println("333");
+                    return r + 1;
+                } else {
+                    System.out.println(e.getMessage());
+                    throw new RuntimeException((e.getMessage()));
+                }
+            }).whenComplete((r, e) -> {
+                if (e == null) {
+                    System.out.println(r);
+                }
+            }).exceptionally((e) -> {
+                System.out.println(e.getMessage());
+                return null;
+            });
+        } finally {
+            pool.shutdown();
+        }
+    }
 
+    @Test
+    public void consumeTest() {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                System.out.println("111");
+                return 1;
+            }, pool).thenAccept((r) -> {
+                System.out.println(r);
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("thenRun");
+            }).exceptionally((e) -> {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+                return null;
+            });
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+    @Test
+    public void threadPoolThenRunTest() throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        CountDownLatch latch = new CountDownLatch(3);
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+                return "";
+            }, pool).thenRun(() -> {
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+            }).thenRun(() -> {
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+            });
+        } finally {
+            pool.shutdown();
+        }
+        latch.await();
+    }
+
+    @Test
+    public void threadPoolThenRunAsyncTest() throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        CountDownLatch latch = new CountDownLatch(3);
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+                return "";
+            }, pool).thenRunAsync(() -> {
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+            }).thenRunAsync(() -> {
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+            });
+        } finally {
+            pool.shutdown();
+        }
+        latch.await();
+    }
+
+    @Test
+    public void applyToEitherTest() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName());
+            return "aaa";
+        });
+
+        CompletableFuture<String> cf2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName());
+            return "bbb";
+        });
+
+        CompletableFuture<String> cf3 = cf1.applyToEither(cf2, r -> {
+            return r + " is Winner!";
+        });
+
+        System.out.println(cf3.get());
+    }
+
+    @Test
+    public void combineTest() throws InterruptedException, ExecutionException {
+        CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(()->{
+            return 10;
+        }).thenCombine(CompletableFuture.supplyAsync(()->{
+            return 20;
+        }), (x, y) -> {
+            return x + y;
+        });
+        System.out.println(cf.get());
+    }
 
 }
--- java_core_01/docs/JAVA_JUC_ADVANCE.md
+++ java_core_01/docs/JAVA_JUC_ADVANCE.md
@@ -1,5 +1,5 @@
-# CompletableFuture
-## Future
+# 2. CompletableFuture
+## 2.1 Future
 - Future 接口定义了操作异步任务执行一些方法，如获取结果、取消执行、判断是否被取消、判断是否完毕等, 表示对异步处理的操作
 - FutureTask 与 Future 的关系: 
   - `public class FutureTask<V> implements RunnableFuture<V>` 
@@ -19,8 +19,7 @@
   - 任务阶段组合
   - 最快结果筛选
 
-## CompletableFuture
-### 基础原理
+## 2.2 CompletableFuture 基本概念
 - 设计原理: 观察者模式, 任务完成后通知监听一方
 - 类架构: 
   - `public class CompletableFuture<T> implements Future<T>, CompletionStage<T>` 
@@ -32,7 +31,7 @@
   - `public CompletableFuture<T> whenComplete(BiConsumer<? super T, ? super Throwable> action)`
   - `public CompletableFuture<T> exceptionally(Function<Throwable, ? extends T> fn)`
  
-### 流式调用
+## 2.3 流式调用
 - 函数式接口:
 
 ![](image/JUC_function.png)
@@ -57,13 +56,26 @@ class Foo {
 }
 ```
 
-### 步骤接口分类
+## 2.4 步骤接口分类
 - 获取结果:
-  - `public T get() throws InterruptedException, ExecutionException`
-  - `public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException`
-  - `public T join()`
-  - `public T getNow(T valueIfAbsent)`
+  - `public T get() throws` 阻塞获取结果
+  - `public T get(long timeout, TimeUnit unit) throws` 预计时间阻塞, 会报超时异常
+  - `public T join()` 抛出的是 uncheck 异常
+  - `public T getNow(T valueIfAbsent)` 不阻塞, 如果未完成则返回 valueIfAbsent 
+  - `public boolean complete(T value)` 直接完成, 如果源未完成返回 value
 - 结果处理: 
-  - `public <U> CompletableFuture<U> thenApply( Function<? super T,? extends U> fn)`
-  - 
+  - `public <U> CompletableFuture<U> thenApply( Function<? super T,? extends U> fn)` 串行化, 会报错并终止
+  - `public <U> CompletableFuture<U> handle( BiFunction<? super T, Throwable, ? extends U> fn)` 串行化, 附带异常传递
+  - `public CompletableFuture<T> exceptionally( Function<Throwable, ? extends T> fn)` 异常处理, 与 handle 类似
+- 结果消费:  
+  - `public CompletableFuture<Void> thenAccept(Consumer<? super T> action)` 无返回值, null
+  - `public CompletableFuture<Void> thenRun(Runnable action)` 无返回值, null
+- 线程池选择:     
+  - 默认线程池: thenRun 和 thenRunAsync 都用默认
+  - 自定义: thenRun 共用自定义线程池, thenRunAsync 调用自定义线程池, 某种情况下会出现优化为 main 线程
+- 速度选择:  
+  - `public <U> CompletableFuture<U> applyToEither(CompletionStage<? extends T> other, Function<? super T, U> fn)` 采用最先算出的 stage
+- 结果合并:
+  - `public <U,V> CompletableFuture<V> thenCombine(CompletionStage<? extends U> other, BiFunction<? super T,? super U,? extends V> fn)`  合并计算结果
+
     
