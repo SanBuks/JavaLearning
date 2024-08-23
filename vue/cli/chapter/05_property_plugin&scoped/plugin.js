8b11b7d42322925273c3246985e86556cce79b7f
sanbuks <sandbuks@gmail.com>
2024-08-23 11:34:19 +0800

    refactor(vue): removed

--- vue/cli/chapter/05_property_plugin&scoped/plugin.js
+++
@@ -1,9 +0,0 @@
-import { commonMounted } from '@/mixin'
-
-export default {
-  install(Vue) {
-    console.log('@@' + '开始增强')
-    Vue.mixin(commonMounted)
-    Vue.prototype.hello = () => {alert('原型方法')}
-  },
-}
--- vue/cli/chapter/12_todolist_event_bus/constants/bus_event_name.js
+++
@@ -1,7 +0,0 @@
-const EVENT_CHANGE_ITEM = "EventChangeItem"
-const EVENT_REMOVE_ITEM = "EventRemoveItem"
-
-export {
-  EVENT_CHANGE_ITEM,
-  EVENT_REMOVE_ITEM
-}
